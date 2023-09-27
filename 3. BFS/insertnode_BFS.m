955c7d267de03a23fdc148f709acf19c77e3c37b
paulamortega <146095664+paulamortega@users.noreply.github.com>
2023-09-26 21:28:06 -0500

    Delete 3. BFS directory
    Cambio de archivo

--- 3. BFS/insertnode_BFS.m
+++
@@ -1,7 +0,0 @@
-function[closed]=insertnode_BFS(closed,current)
-
-%% Inserta al final de la lista close
-
-closed(:,size(closed,2)+1)=current; %Se ingresa el punto actual en la cola de la lista close
-
-end
