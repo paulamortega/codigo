0bff44d06a951521373a6380d592ca998633f164
paulamortega <146095664+paulamortega@users.noreply.github.com>
2023-09-26 21:29:23 -0500

    Delete 6. D Star Nuevo directory
    cambio de archivo

--- 6. D Star Nuevo/calcH_D_Star.m
+++
@@ -1,6 +0,0 @@
-function xnext=calcH_D_Star(xnext, goal)
-
-H=(abs(xnext.y-goal.y)+abs(xnext.x-goal.x))*10; %Se calcula la heurística del xnext 
-xnext.h=H; %Se le asigna el vaor de H a xnext
-
-end
--- 6. D Star Nuevo/calcKey.m
+++
@@ -1,5 +0,0 @@
-function key=calcKey(node)
-
-key=[min(node.g,node.rhs)+node.h, min(node.g,node.rhs)];
-
-end
